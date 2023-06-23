f186d07cd8daf3592f60ff37830839d60cd55335
Skowoo <skowoo@tlen.pl>
2023-06-23 11:46:04 +0200

    Finished XML commentary for DocumentAssistantLibrary

--- DocumentAssistantLibrary/MainContext.cs
+++ DocumentAssistantLibrary/MainContext.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DocumentAssistantLibrary
 {
+    /// <summary>
+    /// Main context to connect and operate on DocumentAssistantDB
+    /// </summary>
     public class MainContext : DbContext
     {
         private static DbContextOptionsBuilder<MainContext> defaultOptions => new DbContextOptionsBuilder<MainContext>()
@@ -13,18 +16,39 @@ namespace DocumentAssistantLibrary
                     errorNumbersToAdd: null)
                     );
 
+        /// <summary>
+        /// Parameterless constructor - uses defaultOptions specified inside MainContext class
+        /// </summary>
         public MainContext() : base(defaultOptions.Options) { }
 
+        /// <summary>
+        /// List of Customers stored in DB
+        /// </summary>
         public DbSet<Customer> Customers { get; set; }
 
+        /// <summary>
+        /// List of Documents stored in DB
+        /// </summary>
         public DbSet<Document> Documents { get; set; }
 
+        /// <summary>
+        /// List of Document types stored in DB
+        /// </summary>
         public DbSet<DocumentType> DocumentTypes { get; set; }
 
+        /// <summary>
+        /// List of Roles stored in DB
+        /// </summary>
         public DbSet<Role> Roles { get; set; }
 
+        /// <summary>
+        /// List of Users stored in DB
+        /// </summary>
         public DbSet<User> Users { get; set; }
 
+        /// <summary>
+        /// List of Languages stored in DB
+        /// </summary>
         public DbSet<Language> Languages { get; set; }
     }
 }
