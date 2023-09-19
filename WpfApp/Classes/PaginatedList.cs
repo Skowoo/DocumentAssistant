11c2d0b2c0a01095737ad31de28b2b784509d689
Skowoo <skowoo@tlen.pl>
2023-09-19 10:04:05 +0200

    Created PaginatedList Class

---
+++ WpfApp/Classes/PaginatedList.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Classes
+{
+    internal class PaginatedList<T> : List<T>
+    {
+
+        public int PagesCount { get; private set; }
+
+        public int CurrentPage { get; private set; }
+
+        public PaginatedList(List<T> items, int count, int requestedPage, int totalPagesCount)
+        {
+            CurrentPage = requestedPage;
+            PagesCount = (int)Math.Ceiling(count / (double)totalPagesCount);
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage => CurrentPage > 1;
+
+        public bool HasNextPage => CurrentPage < PagesCount;
+
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
